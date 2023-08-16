eac004222939eeb091b07ab15a42dd73346de1b7
sinryuji <siler2474@gmail.com>
2023-08-16 19:15:49 +0900

    test: UserPushKeyword deleteByUserIdAndKeyword Test

--- backend/src/test/java/peer/backend/keyword/UserPushKeywordRepositoryTest.java
+++ backend/src/test/java/peer/backend/keyword/UserPushKeywordRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.convert.DataSizeUnit;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.time.LocalDateTime;
@@ -74,5 +75,19 @@ public class UserPushKeywordRepositoryTest {
         List<UserPushKeyword> findList = userPushKeywordRepository.findAllByUserId(user.getId());
         assertEquals(findList.size(), 1);
         assertEquals(findList.get(0).getKeyword(), "hello");
+    }
+
+    @Test
+    @DisplayName("UserPushKeyword deleteByUserIdAndKeyword 테스트")
+    void deleteTest() {
+        User user = this.userRepository.findAll().get(0);
+        UserPushKeyword userPushKeyword = UserPushKeyword.builder()
+            .user(user)
+            .keyword("hello").build();
+        userPushKeywordRepository.save(userPushKeyword);
+        userPushKeywordRepository.deleteByUserIdAndKeyword(user.getId(),
+            userPushKeyword.getKeyword());
+        List<UserPushKeyword> findList = userPushKeywordRepository.findAllByUserId(user.getId());
+        assertEquals(findList.size(), 0);
     }
 }
