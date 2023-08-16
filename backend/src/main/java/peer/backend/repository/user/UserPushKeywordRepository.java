89cdc4e6c901740f53d9481c737705b88558c893
sinryuji <siler2474@gmail.com>
2023-08-16 18:13:02 +0900

    test: UserPushKeywordRepositoryTest

--- backend/src/main/java/peer/backend/repository/user/UserPushKeywordRepository.java
+++ backend/src/main/java/peer/backend/repository/user/UserPushKeywordRepository.java
@@ -1,8 +1,10 @@
 package peer.backend.repository.user;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import peer.backend.entity.user.UserPushKeyword;
 
 public interface UserPushKeywordRepository extends JpaRepository<UserPushKeyword, Long> {
 
+    public List<UserPushKeyword> findAllByUserId(Long userId);
 }
