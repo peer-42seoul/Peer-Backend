472c59213873c3d8bd12cd6b0ff50b9ae806d211
sinryuji <siler2474@gmail.com>
2023-09-30 15:26:38 +0900

    chore: spring batch, mongo setting

--- backend/src/main/java/peer/backend/BackendApplication.java
+++ backend/src/main/java/peer/backend/BackendApplication.java
@@ -1,9 +1,12 @@
 package peer.backend;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableBatchProcessing
 @EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
