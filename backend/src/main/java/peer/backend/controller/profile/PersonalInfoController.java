be9da4088374e24946472e0e1921200c12634edc
Leenux <leenux@Leenuxui-Macmini.local>
2023-10-11 00:15:38 +0900

    feat: C-MYPAGE-11 change password

--- backend/src/main/java/peer/backend/controller/profile/PersonalInfoController.java
+++ backend/src/main/java/peer/backend/controller/profile/PersonalInfoController.java
@@ -7,7 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import peer.backend.dto.profile.PasswordRequest;
 import peer.backend.service.profile.PersonalInfoService;
 
 @Controller
@@ -19,5 +22,12 @@ public class PersonalInfoController {
     @GetMapping("/info")
     public ResponseEntity<Object> getPersonalInfo(Authentication auth) {
         return new ResponseEntity<> (personalInfoService.getPersonalInfo(auth.getName()), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "C-MYPAGE-11", notes = "사용자 개인정보 비밀번호 변경하기")
+    @PutMapping("/info/password")
+    public ResponseEntity<Object> changePassword(Authentication auth, @RequestBody PasswordRequest passwords) {
+        personalInfoService.changePassword(auth.getName(), passwords);
+        return new ResponseEntity<> (HttpStatus.CREATED);
     }
 }
--- backend/src/main/java/peer/backend/service/profile/PersonalInfoService.java
+++ backend/src/main/java/peer/backend/service/profile/PersonalInfoService.java
@@ -1,10 +1,13 @@
 package peer.backend.service.profile;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import peer.backend.dto.profile.PasswordRequest;
 import peer.backend.dto.profile.PersonalInfoResponse;
 import peer.backend.entity.user.User;
+import peer.backend.exception.ForbiddenException;
 import peer.backend.exception.NotFoundException;
 import peer.backend.repository.user.UserRepository;
 
@@ -25,5 +28,21 @@ public class PersonalInfoService {
                 .local(user.getAddress())
                 .authentication(user.getCompany())
                 .build();
+    }
+
+    @Transactional
+    public void changePassword(String email, PasswordRequest passwords) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new NotFoundException("사용자를 찾을 수 없습니다.")
+        );
+        if (!encoder.matches(passwords.getPresentPassword(), user.getPassword())) {
+            throw new ForbiddenException("존재하지 않는 비밀번호 입니다.");
+        }
+        if (!passwords.getNewPassword().equals(passwords.getConfirmPassword())) {
+            throw new ForbiddenException("비밀번호가 일치하지 않습니다.");
+        }
+        user.setPassword(encoder.encode(passwords.getNewPassword()));
+        userRepository.save(user);
     }
 }
