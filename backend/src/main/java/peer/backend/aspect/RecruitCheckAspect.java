24f444b0a73a9868b11a40e578b679253daa44df
jwee <jwee@jweeui-MacBookAir.local>
2023-10-27 18:57:08 +0900

    [fix] delete System.out. for bug fix

--- backend/src/main/java/peer/backend/aspect/RecruitCheckAspect.java
+++
@@ -1,9 +0,0 @@
-package peer.backend.aspect;
-
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
-
-@Aspect
-@Component
-public class RecruitCheckAspect {
-}
