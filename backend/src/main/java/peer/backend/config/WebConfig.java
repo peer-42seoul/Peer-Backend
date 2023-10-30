958cca1f3b4a2c366b72cf802344a4ee24a5e85b
sinryuji <siler2474@gmail.com>
2023-10-30 22:40:49 +0900

    fix: cors

--- backend/src/main/java/peer/backend/config/WebConfig.java
+++ backend/src/main/java/peer/backend/config/WebConfig.java
@@ -4,19 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.core.convert.ConverterNotFoundException;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import peer.backend.config.team.StringToTeamStatusConverter;
 import peer.backend.config.team.StringToTeamUserRoleTypeConverter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
-    public void addFormatters(FormatterRegistry registry)  {
+    public void addFormatters(FormatterRegistry registry) {
         try {
             registry.addConverter(new StringToTeamStatusConverter());
             registry.addConverter(new StringToTeamUserRoleTypeConverter());
         } catch (Exception e) {
             throw new RuntimeException();
         }
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*")
+            .allowCredentials(true);
     }
 }
