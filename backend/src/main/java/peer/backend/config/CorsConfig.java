92421e4edc947b6508991faea195b6d1d50e8068
sinryuji <siler2474@gmail.com>
2023-10-30 22:03:00 +0900

    fix: cors filter Order vert first

--- backend/src/main/java/peer/backend/config/CorsConfig.java
+++ backend/src/main/java/peer/backend/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package peer.backend.config;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsConfig {
 
     @Bean
