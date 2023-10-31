89d50d927b938c34dcd5fc4abcdf1eb993ab7ee9
haryu <haryu@student.42seoul.kr>
2023-11-01 04:34:31 +0900

    FIX : remove param userId
    - UserId Param 으로 받던것 삭제
    - 하는 김에 추가로 거슬리던 Validation 어노테이션 하나 추가

--- backend/src/main/java/peer/backend/dto/message/TargetDTO.java
+++ backend/src/main/java/peer/backend/dto/message/TargetDTO.java
@@ -5,11 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class TargetDTO {
+    @NotEmpty
+    @Min(0)
     private long targetId; // 대화 상대
 }
