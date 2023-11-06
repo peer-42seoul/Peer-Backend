764c5d75154fc8a3fe70e63dc92b8a66edc35983
jwee <jwee@jweeui-MacBookAir.local>
2023-11-06 12:26:49 +0900

    fix : delete comment

--- backend/src/main/java/peer/backend/dto/board/recruit/RecruitRequest.java
+++ backend/src/main/java/peer/backend/dto/board/recruit/RecruitRequest.java
@@ -10,9 +10,6 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RecruitRequest {
-
-//    /recruitement?type=study&sort=latest&&page=1&pagesize=9&keword="키워드"&due=1&region="김포"
-//            &place="online"&status="ongoing"&tag="java,spring,react"
     private String type;
     private String sort;
     private Long page;
