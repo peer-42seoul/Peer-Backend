6d81586e92b1fb3fbbad5d283b78ae3732ada45c
이주현 <juhyelee@ijuhyeon-ui-MacBookAir.local>
2023-11-10 16:22:26 +0900

    fix: change type and add recruit repository

--- backend/src/main/java/peer/backend/repository/board/recruit/RecruitFavoriteRepository.java
+++ backend/src/main/java/peer/backend/repository/board/recruit/RecruitFavoriteRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import peer.backend.entity.board.recruit.RecruitFavorite;
 import peer.backend.entity.composite.RecruitFavoritePK;
 
+import java.util.List;
+
 public interface RecruitFavoriteRepository extends JpaRepository<RecruitFavorite, RecruitFavoritePK> {
+    List<RecruitFavorite> findAllByUserId(Long userId);
 }
