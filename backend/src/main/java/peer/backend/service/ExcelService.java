087eb384041a32e92bb1343280276b7caa4faff9
sinryuji <siler2474@gmail.com>
2023-11-13 19:55:49 +0900

    feat: TeamTracking Excel Download

--- backend/src/main/java/peer/backend/service/ExcelService.java
+++ backend/src/main/java/peer/backend/service/ExcelService.java
@@ -15,7 +15,9 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
+import peer.backend.mongo.entity.TeamTracking;
 import peer.backend.mongo.entity.UserTracking;
+import peer.backend.mongo.repository.TeamTrackingRepository;
 import peer.backend.mongo.repository.UserTrackingRepository;
 
 @RequiredArgsConstructor
@@ -23,9 +25,11 @@ import peer.backend.mongo.repository.UserTrackingRepository;
 public class ExcelService {
 
     private final UserTrackingRepository userTrackingRepository;
+    private final TeamTrackingRepository teamTrackingRepository;
 
     public ByteArrayInputStream getTrackingExcel() throws IOException {
         List<UserTracking> userTrackingList = this.userTrackingRepository.findAll();
+        List<TeamTracking> teamTrackingList = this.teamTrackingRepository.findAll();
 
         Workbook workbook = new XSSFWorkbook();
         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -39,8 +43,11 @@ public class ExcelService {
         headerCellStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
         headerCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
-        Sheet sheet = workbook.createSheet("UserTracking");
-        createUserTrackingSheet(userTrackingList, sheet, headerCellStyle);
+        Sheet userTrackingSheet = workbook.createSheet("UserTracking");
+        createUserTrackingSheet(userTrackingList, userTrackingSheet, headerCellStyle);
+
+        Sheet teamTrackingSheet = workbook.createSheet("TeamTracking");
+        createTeamTrackingSheet(teamTrackingList, teamTrackingSheet, headerCellStyle);
 
         workbook.write(out);
         return new ByteArrayInputStream(out.toByteArray());
@@ -86,6 +93,54 @@ public class ExcelService {
             bodyCell.setCellValue(userTracking.getStatus().getValue());
             bodyCell = bodyRow.createCell(10);
             bodyCell.setCellValue(userTracking.getReportCount());
+        }
+
+        for (int i = 0; i < headers.length; i++) {
+            sheet.autoSizeColumn(i);
+            sheet.setColumnWidth(i, (sheet.getColumnWidth(i) + 1024));
+        }
+    }
+
+    private void createTeamTrackingSheet(List<TeamTracking> list, Sheet sheet,
+        CellStyle headerCellStyle) {
+        Row headerRow = sheet.createRow(0);
+        String[] headers = {"teamId", "teamName", "actionDate", "actionType",
+            "tag", "actionFinishedDate", "actionUnproperFinishedDate", "teamStatus",
+            "42Subject", "In-42"};
+
+        for (int i = 0; i < headers.length; i++) {
+            Cell headerCell = headerRow.createCell(i);
+            headerCell.setCellValue(headers[i]);
+            headerCell.setCellStyle(headerCellStyle);
+        }
+
+        int idx = 1;
+        for (TeamTracking teamTracking : list) {
+            Row bodyRow = sheet.createRow(idx++);
+            Cell bodyCell = bodyRow.createCell(0);
+            bodyCell.setCellValue(teamTracking.getTeamId());
+            bodyCell = bodyRow.createCell(1);
+            bodyCell.setCellValue(teamTracking.getTeamName());
+            bodyCell = bodyRow.createCell(2);
+            bodyCell.setCellValue(teamTracking.getActionDate().toString());
+            bodyCell = bodyRow.createCell(3);
+            bodyCell.setCellValue(teamTracking.getActionType().getValue());
+            bodyCell = bodyRow.createCell(4);
+            bodyCell.setCellValue(teamTracking.getTag());
+            bodyCell = bodyRow.createCell(5);
+            if (teamTracking.getActionFinishedDate() != null) {
+                bodyCell.setCellValue(teamTracking.getActionFinishedDate().toString());
+            }
+            bodyCell = bodyRow.createCell(6);
+            if (teamTracking.getActionUnproperFinishedDate() != null) {
+                bodyCell.setCellValue(teamTracking.getActionUnproperFinishedDate().toString());
+            }
+            bodyCell = bodyRow.createCell(7);
+            bodyCell.setCellValue(teamTracking.getTeamStatus().getValue());
+            bodyCell = bodyRow.createCell(8);
+            bodyCell.setCellValue(teamTracking.isFtSubject());
+            bodyCell = bodyRow.createCell(9);
+            bodyCell.setCellValue(teamTracking.getIn42());
         }
 
         for (int i = 0; i < headers.length; i++) {
