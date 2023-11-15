3212b35e0dea3ac4f2e26a81a9588d5032fe2d33
haryu <haryu@student.42seoul.kr>
2023-11-15 15:11:45 +0900

    WIP: socket connection port change 8081

--- backend/src/main/java/peer/backend/config/SocketIoConfig.java
+++ backend/src/main/java/peer/backend/config/SocketIoConfig.java
@@ -4,6 +4,7 @@ package peer.backend.config;
 import com.corundumstudio.socketio.SocketConfig;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
 @CrossOrigin
+@Slf4j
 public class SocketIoConfig {
 
     @Value("${socket.io.port}")
@@ -27,6 +29,7 @@ public class SocketIoConfig {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
         config.setHostname(socketIoHost); // 호스트 설정
         config.setPort(socketIoPort); // 포트 설정
+        log.info("socket / port : " + socketIoHost + " / " + socketIoPort);
         SocketConfig portConfig = new SocketConfig();
         portConfig.setReuseAddress(true);
         config.setSocketConfig(portConfig);
