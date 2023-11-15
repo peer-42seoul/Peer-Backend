1d1f453abf6336ce76bc0c81e399c7c1020b42ac
이주현 <juhyelee@ijuhyeon-ui-MacBookAir.local>
2023-11-15 17:10:17 +0900

    fix: remove hashing count

--- backend/src/main/java/peer/backend/config/JasyptConfig.java
+++ backend/src/main/java/peer/backend/config/JasyptConfig.java
@@ -14,7 +14,6 @@ public class JasyptConfig {
         SimpleStringPBEConfig pbeConfig = new SimpleStringPBEConfig();
         pbeConfig.setPassword("c044471d1c49295387aabfc8d00c72ca918da1bc1fa94a146c45b01c22acf54c");
         pbeConfig.setAlgorithm("PBEWithMD5AndDES");
-        pbeConfig.setKeyObtentionIterations("1000");
         pbeConfig.setPoolSize("1");
         pbeConfig.setProviderName("SunJCE");
         pbeConfig.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
