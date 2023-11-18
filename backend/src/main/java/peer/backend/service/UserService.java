fd501e6855ed615e93b3e916e94b796997c3d57a
sinryuji <siler2474@gmail.com>
2023-11-18 17:06:43 +0900

    refactor: user registration tracking refactoring

--- backend/src/main/java/peer/backend/service/UserService.java
+++ backend/src/main/java/peer/backend/service/UserService.java
@@ -3,7 +3,6 @@ package peer.backend.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import peer.backend.annotation.tracking.UserRegistrationTracking;
 import peer.backend.entity.user.User;
 import peer.backend.repository.user.UserRepository;
 
@@ -13,7 +12,6 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    @UserRegistrationTracking
     public User saveUser(User user) {
         return this.userRepository.save(user);
     }
