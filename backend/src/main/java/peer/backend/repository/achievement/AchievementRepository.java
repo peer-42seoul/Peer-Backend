e7cf800116c0cd93bf385ae1735ba01b6e72b753
haryu <haryu@student.42seoul.kr>
2023-11-26 06:31:05 +0900

    wip

--- backend/src/main/java/peer/backend/repository/achievement/AchievementRepository.java
+++
@@ -1,8 +0,0 @@
-package peer.backend.repository.achievement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import peer.backend.entity.achievement.Achievement;
-
-public interface AchievementRepository extends JpaRepository<Achievement, Long> {
-
-}
