9232bb12086ae4b465fa38684dee5af29c8a7103
sinryuji <siler2474@gmail.com>
2023-12-01 18:24:00 +0900

    feat: tag add role check

--- backend/src/main/java/peer/backend/entity/Tag.java
+++ backend/src/main/java/peer/backend/entity/Tag.java
@@ -7,12 +7,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "tag")
 public class Tag {
 
