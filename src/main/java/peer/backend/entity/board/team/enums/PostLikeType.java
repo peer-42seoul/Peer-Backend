ad64440eff6466e82a0b06e8465df67dc5e7fad9
weejihye <kikiwjh@naver.com>
2023-12-04 22:27:16 +0900

    fix: delete unused import

--- src/main/java/peer/backend/entity/board/team/enums/PostLikeType.java
+++ src/main/java/peer/backend/entity/board/team/enums/PostLikeType.java
@@ -1,8 +1,6 @@
 package peer.backend.entity.board.team.enums;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
