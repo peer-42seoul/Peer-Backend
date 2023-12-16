0f56ceadb4667cf257cf5dea433a44d43cf40ccf
wochae <officialwochae@gmail.com>
2023-12-17 00:16:29 +0900

    refactor: renamed from noti to notice #484

--- src/main/java/peer/backend/controller/NoticeController.java
+++ src/main/java/peer/backend/controller/NoticeController.java
@@ -4,39 +4,34 @@ package peer.backend.controller;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import peer.backend.dto.alarm.AlarmDto;
 import peer.backend.entity.alarm.Alarm;
-import peer.backend.entity.user.User;
+import peer.backend.service.alarm.AlarmAdminService;
 import peer.backend.service.alarm.AlarmService;
 
 @RequiredArgsConstructor
 @RestController
 @Slf4j
-@RequestMapping("/api/v1/noti")
-
-public class NotiController {
+public class NoticeController {
     private final AlarmService alarmService;
+    private final AlarmAdminService alarmAdminService;
 
-    @PutMapping("spring")
-    public ResponseEntity<Object> saveAlarm(@RequestBody AlarmDto dto, Authentication auth) {
+    @PutMapping("/api/v1/admin/noti/save")
+    public ResponseEntity<Object> saveAlarmAdmin(@RequestBody AlarmDto dto) {
         Alarm alarm = alarmService.alarmFromDto(dto);
-        User user = User.authenticationToUser(auth);
         alarmService.saveAlarm(alarm);
         return ResponseEntity.ok().build();
     }
-
-    @GetMapping("spring")
-    public ResponseEntity<Object> getAlarm(@RequestParam(value = "target") Long target) {
-        List<Alarm> alarms = alarmService.getAlarm(target);
+    @GetMapping("/api/v1/admin/noti/spring")
+    public ResponseEntity<Object> getAlarm(@RequestParam(value = "type") String type) {
+        List<Alarm> alarms = alarmAdminService.getAlarm(type);
         return ResponseEntity.ok().body(alarms);
     }
 
