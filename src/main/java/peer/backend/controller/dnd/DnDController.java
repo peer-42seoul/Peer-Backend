a1e8dc63b0b2d28fd31a68668854cda1f48b2df9
hansol <ryu.axel@gmail.com>
2023-12-25 14:36:03 +0900

    WIP

---
+++ src/main/java/peer/backend/controller/dnd/DnDController.java
@@ -0,0 +1,61 @@
+package peer.backend.controller.dnd;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import peer.backend.dto.temp.RequestDnDDTO;
+import peer.backend.mongo.entity.TeamDnD;
+import peer.backend.service.dnd.DnDService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/dnd-main")
+public class DnDController {
+    private final DnDService dndService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody TeamDnD data) {
+        TeamDnD ret;
+        try {
+            ret = this.dndService.createDnD(data);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<TeamDnD>(ret,HttpStatus.CREATED);
+    }
+
+    @PostMapping("/read")
+    public ResponseEntity<?> read(@RequestBody RequestDnDDTO data) {
+        TeamDnD ret;
+        try {
+            ret = this.dndService.getDnD(data);
+            if (ret == null)
+                return new ResponseEntity<>("There is no that DnD data", HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<TeamDnD>(ret,HttpStatus.OK);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<?> update(@RequestBody TeamDnD data) {
+        TeamDnD ret;
+        try{
+            ret = this.dndService.updateDnD(data);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<TeamDnD>(ret, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> delete(@RequestBody RequestDnDDTO data) {
+        try {
+            this.dndService.deleteDnD(data);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
