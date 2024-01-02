9867ee3962c3667c3301a0e37b484582f30acaae
sinryuji <siler2474@gmail.com>
2024-01-02 20:50:11 +0900

    feat: get team default list api

--- src/main/java/peer/backend/repository/team/TeamRepository.java
+++ src/main/java/peer/backend/repository/team/TeamRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import peer.backend.entity.team.Team;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
     Optional<Team> findByName(String name);
 }
