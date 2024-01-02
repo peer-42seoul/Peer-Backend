222e6dde605c8dd5c206d0c4c5c971bd5389931b
sinryuji <siler2474@gmail.com>
2024-01-02 14:49:43 +0900

    feat: admin get user default list api

---
+++ src/main/java/peer/backend/controller/AdminController.java
@@ -0,0 +1,26 @@
+package peer.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import peer.backend.dto.user.UserDefaultResponse;
+import peer.backend.entity.user.User;
+import peer.backend.service.UserService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+
+    private final UserService userService;
+
+    @GetMapping("user")
+    public ResponseEntity<Page<UserDefaultResponse>> getUserDefaultList(Pageable pageable) {
+        Page<User> userList = this.userService.getUserListFromPageable(pageable);
+        return ResponseEntity.ok().body(userList.map(UserDefaultResponse::new));
+    }
+}
---
+++ src/main/java/peer/backend/dto/user/UserDefaultResponse.java
@@ -0,0 +1,20 @@
+package peer.backend.dto.user;
+
+import lombok.Getter;
+import peer.backend.entity.user.User;
+
+@Getter
+public class UserDefaultResponse {
+
+    private final Long userId;
+    private final String nickname;
+    private final String email;
+    private final String name;
+
+    public UserDefaultResponse(User user) {
+        this.userId = user.getId();
+        this.nickname = user.getNickname();
+        this.email = user.getEmail();
+        this.name = user.getName();
+    }
+}
