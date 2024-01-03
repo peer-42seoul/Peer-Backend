ca04499f3ead15f6a6dc4eede01d9c9c2758a890
sinryuji <siler2474@gmail.com>
2024-01-03 17:09:50 +0900

    feat: get reserved alarm list

--- src/main/java/peer/backend/controller/AdminAlarmController.java
+++ src/main/java/peer/backend/controller/AdminAlarmController.java
@@ -1,12 +1,16 @@
 package peer.backend.controller;
 
+import java.awt.print.Pageable;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import peer.backend.dto.adminAlarm.AlarmListResponse;
 import peer.backend.dto.adminAlarm.SendAlarmRequest;
 
 @RestController
@@ -19,6 +23,12 @@ public class AdminAlarmController {
     @PostMapping
     public ResponseEntity<Void> sendAlarm(@RequestBody @Valid SendAlarmRequest request) {
         // TODO: calling a function that send notification
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<AlarmListResponse>> getReservedAlarmList(Pageable pageable) {
+        // TODO: get a reserved alarm list and mapping to dto
         return ResponseEntity.ok().build();
     }
 }
