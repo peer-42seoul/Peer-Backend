8edec3554067e281710aa61ddcdbd6f3c987c9a2
weejihye <kikiwjh@naver.com>
2024-01-08 20:25:28 +0900

    hotfix-team-get-setting-dto

--- src/main/java/peer/backend/dto/team/TeamMemberDto.java
+++ src/main/java/peer/backend/dto/team/TeamMemberDto.java
@@ -4,15 +4,24 @@ import lombok.Getter;
 import peer.backend.entity.team.TeamUser;
 import peer.backend.entity.team.enums.TeamUserRoleType;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 public class TeamMemberDto {
     private Long id;
     private String name;
     private TeamUserRoleType role;
+    private List<String> job;
 
     public TeamMemberDto(TeamUser teamUser) {
         this.id = teamUser.getUserId();
         this.name = teamUser.getUser().getNickname();
         this.role = teamUser.getRole();
+        this.job = teamUser.getTeamUserJobs()
+                .stream()
+                .map(
+                        teamUserJob -> teamUserJob.getTeamJob().getName())
+                .collect(Collectors.toList());
     }
 }
