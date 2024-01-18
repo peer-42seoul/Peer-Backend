08b5e2c35ec516aa3710ac1e1c6933ca3d2a9257
wochae <officialwochae@gmail.com>
2024-01-18 22:10:20 +0900

    [Feat] : 팀 생성 시 공지사항 게시판 생성 #625
    팀 생성 로직에서 해당 메서드 호출하도록.

---
+++ src/main/java/peer/backend/service/board/team/NoticeService.java
@@ -0,0 +1,37 @@
+package peer.backend.service.board.team;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import peer.backend.dto.board.team.BoardCreateRequest;
+import peer.backend.entity.board.team.Board;
+import peer.backend.entity.board.team.enums.BoardType;
+import peer.backend.entity.team.Team;
+import peer.backend.entity.user.User;
+import peer.backend.exception.ConflictException;
+import peer.backend.exception.NotFoundException;
+import peer.backend.repository.board.team.BoardRepository;
+import peer.backend.repository.team.TeamRepository;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeService {
+
+    private final TeamRepository teamRepository;
+    private final BoardRepository boardRepository;
+    @Transactional
+    public void createNoticeBoard(Long teamId) {
+
+        Team team = teamRepository.findById(teamId).orElseThrow(
+                () -> new NotFoundException("존재하지 않는 팀입니다."));
+        Board board = Board.builder()
+                .team(team)
+                .name("공지사항")
+                .type(BoardType.NOTICE)
+                .build();
+        boardRepository.save(board);
+    }
+
+}
