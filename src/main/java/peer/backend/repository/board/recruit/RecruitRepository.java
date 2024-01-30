aa91a128acd66e09305c821c349f3cf19571bcfd
weejihye <kikiwjh@naver.com>
2024-01-30 14:08:41 +0900

    fix: modify query for exclude recruit including favorites

--- src/main/java/peer/backend/repository/board/recruit/RecruitRepository.java
+++ src/main/java/peer/backend/repository/board/recruit/RecruitRepository.java
@@ -9,6 +9,6 @@ import peer.backend.entity.board.recruit.enums.RecruitStatus;
 import peer.backend.entity.team.enums.TeamType;
 
 public interface RecruitRepository extends JpaRepository<Recruit, Long> {
-    @Query("SELECT r FROM Recruit r JOIN r.team t WHERE t.type = :type AND r.status = :status AND NOT EXISTS (SELECT f FROM RecruitFavorite f WHERE f.recruit = r AND f.type <> 'LIKE')")
+    @Query("SELECT r FROM Recruit r JOIN r.team t WHERE t.type = :type AND r.status = :status AND NOT EXISTS (SELECT f FROM RecruitFavorite f WHERE f.recruit = r)")
     Page<Recruit> findAllByStatusAndTeamTypeAndFavorite(RecruitStatus status, TeamType type, Pageable pageable);
 }
