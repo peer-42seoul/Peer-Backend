86fe643efb676d6e4749f561528197e1e190aea8
weejihye <kikiwjh@naver.com>
2024-01-30 20:13:11 +0900

    feat : add getFavoriteList of showcase

--- src/main/java/peer/backend/service/profile/FavoriteService.java
+++ src/main/java/peer/backend/service/profile/FavoriteService.java
@@ -1,25 +1,22 @@
 package peer.backend.service.profile;
 
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.security.core.Authentication;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import peer.backend.dto.board.recruit.RecruitListResponse;
-import peer.backend.dto.profile.FavoritePage;
 import peer.backend.dto.profile.response.RecruitFavoriteDto;
-import peer.backend.entity.board.recruit.Recruit;
+import peer.backend.dto.profile.response.ShowcaseFavoriteResponse;
 import peer.backend.entity.board.recruit.RecruitFavorite;
 import peer.backend.entity.board.recruit.enums.RecruitFavoriteEnum;
+import peer.backend.entity.board.team.Post;
+import peer.backend.entity.board.team.enums.BoardType;
+import peer.backend.entity.board.team.enums.PostLikeType;
 import peer.backend.entity.team.enums.TeamType;
 import peer.backend.entity.user.User;
 import peer.backend.repository.board.recruit.RecruitFavoriteRepository;
-import peer.backend.repository.board.recruit.RecruitRepository;
+import peer.backend.repository.board.team.PostRepository;
 import peer.backend.service.TagService;
 
 @Service
@@ -27,6 +24,8 @@ import peer.backend.service.TagService;
 public class FavoriteService {
 
     private final RecruitFavoriteRepository recruitFavoriteRepository;
+    private final PostRepository postRepository;
+    private final TagService tagService;
 
     @Transactional(readOnly = true)
     public Page<RecruitFavoriteDto> getFavorite(User user, String type, int pageIndex, int pageSize) {
@@ -46,5 +45,21 @@ public class FavoriteService {
                         user.getId(),
                         RecruitFavoriteEnum.LIKE,
                         TeamType.from(type));
+    }
+
+    @Transactional
+    public Page<ShowcaseFavoriteResponse> getShowcaseFavorites(User user, Pageable pageable) {
+        Page<Post> favorites = postRepository.findShowcaseFavoriteList(
+                true,
+                BoardType.SHOWCASE,
+                user.getId(),
+                PostLikeType.FAVORITE,
+                pageable
+        );
+        return favorites.map(favorite ->
+                new ShowcaseFavoriteResponse(
+                        favorite, tagService.recruitTagListToTagResponseList(
+                                favorite.getBoard().getTeam().getRecruit().getRecruitTags()))
+        );
     }
 }
