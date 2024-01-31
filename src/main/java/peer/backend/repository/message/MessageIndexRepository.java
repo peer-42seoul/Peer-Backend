1aa41f48d04168e25ffc2fff7123e8d377980034
hansol <ryu.axel@gmail.com>
2024-01-31 12:25:52 +0900

    FEAT: finish message new api

--- src/main/java/peer/backend/repository/message/MessageIndexRepository.java
+++ src/main/java/peer/backend/repository/message/MessageIndexRepository.java
@@ -16,6 +16,11 @@ public interface MessageIndexRepository extends JpaRepository<MessageIndex, Long
 
     Optional<MessageIndex> findTopByConversationId(long conversationId);
 
-    @Query("SELECT m FROM MessageIndex m WHERE (m.userIdx1 = : userId1 AND m.userIdx2 = :userId2) OR (m.userIdx1 = :userId2 AND m.userIdx2 = :userId1)")
+    @Query("SELECT m FROM MessageIndex m WHERE " +
+            "((m.userIdx1 = : userId1 AND m.userIdx2 = :userId2)" +
+            " OR " +
+            "(m.userIdx1 = :userId2 AND m.userIdx2 = :userId1))" +
+            " AND " +
+            "(m.user1delete = false AND m.user2delete = false)")
     Optional<MessageIndex> findByUserIdx1AndUserIdx2(Long userId1, Long userId2);
 }
