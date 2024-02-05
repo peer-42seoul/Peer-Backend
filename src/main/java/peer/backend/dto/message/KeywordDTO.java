170f52245c288d9734ff7dbfb3d46e7adcb6db9b
hansol <ryu.axel@gmail.com>
2024-02-05 20:30:26 +0900

    ADD: new regexp

--- src/main/java/peer/backend/dto/message/KeywordDTO.java
+++ src/main/java/peer/backend/dto/message/KeywordDTO.java
@@ -13,6 +13,6 @@ public class KeywordDTO {
 
     @NotBlank(message = "검색할 내용을 추가하세요.")
     @Size(min=2, max=10, message = "검색 키워드는 최소 2글자, 최대 10자까지 검색이 가능합니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "키워드 검색시 특수문자는 사용하실 수 없습니다")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$", message = "키워드 검색시 특수문자는 사용하실 수 없습니다")
     private String keyword; // 대화 상대
 }
