27e26bc01eb92bffaa5988056c76c55eac8cd58c
sinryuji <siler2474@gmail.com>
2024-02-14 15:03:42 +0900

    feat: upload admin images

--- src/main/java/peer/backend/controller/object/ObjectController.java
+++ src/main/java/peer/backend/controller/object/ObjectController.java
@@ -1,13 +1,15 @@
 package peer.backend.controller.object;
 
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import peer.backend.entity.user.User;
 import peer.backend.service.file.ObjectService;
-
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,10 +18,17 @@ public class ObjectController {
 
     private final ObjectService objectService;
 
-    @PostMapping({"/editor/image", "/admin/editor/image"})
+    @PostMapping({"/editor/image"})
     public String uploadImage(@RequestParam("image") MultipartFile image, Authentication auth)
         throws IOException {
         return objectService.uploadImage(image,
             "editor/" + User.authenticationToUser(auth).getId());
+    }
+
+    @PostMapping("/admin/editor/image")
+    public String uploadAdminImage(@RequestParam("image") MultipartFile image)
+        throws IOException {
+        return objectService.uploadImage(image,
+            "editor/admin");
     }
 }
